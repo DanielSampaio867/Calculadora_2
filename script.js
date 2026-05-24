3deba08e5a663592704fc40016e6eef2e991c901
Daniel Sampaio <dansampaio255@gmail.com>
2026-05-24 14:34:55 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -29,14 +29,24 @@ function receberQuantidadedePessoas(evento){
  }
 }
 
-connst = botoesGorjeta = document.querySelectorll(".gorjeta input[type = 'button']")
+const  botoesGorjeta = document.querySelectorAll(".gorjeta input[type = 'button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagem)
+    botao.addEventListener("click", receberPorcentagemBotao)
 
-} );
+} )
 
-function receberPorcentagem(evento){
+function receberPorcentagemBotao(evento){
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+        
+        
+        if(botao.value === evento.target.value) {
 
+            botao.classList.add("botao-ativo")
+        }
 
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100 
     
 }
